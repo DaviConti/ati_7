30271aca7b99379c68d407613faf6134796a4973
davi_conti <davi_conti@estudante.sesisenai.org.br>
2025-08-26 12:48:52 -0300

    add

---
+++ ati7/public/index.php
@@ -0,0 +1,19 @@
+<?php
+// index.php
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <title>Biblioteca - Sistema</title>
+</head>
+<body>
+  <h1>Sistema da Biblioteca</h1>
+  <ul>
+    <li><a href="read_autores.php">Autores</a></li>
+    <li><a href="read_livros.php">Livros</a></li>
+    <li><a href="read_leitores.php">Leitores</a></li>
+    <li><a href="read_emprestimos.php">Empréstimos</a></li>
+  </ul>
+</body>
+</html>
